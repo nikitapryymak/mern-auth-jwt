c4b20b7962b910f6f9f8c31af3fa9ec4485dd8b2
Nikita Pryymak <nikitapryymak29@gmail.com>
2024-07-04 14:38:29 -0700

    Containerize the API and database. Create a docker-compose file

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,8 +3,9 @@ import { MONGO_URI } from "../constants/env";
 
 const connectToDatabase = async () => {
   try {
+    console.log("Connecting to DB...");
     await mongoose.connect(MONGO_URI);
-    console.log("Successfully connected to DB");
+    console.log("Successfully connected to DB!");
   } catch (error) {
     console.error("Could not connect to DB", error);
     process.exit(1);
